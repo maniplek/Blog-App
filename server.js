e5eb9a30a0d962ac522b1b06f2f0b922a9871e34
Maniple Kwizera <83705949+maniplek@users.noreply.github.com>
2021-11-01 20:05:15 +0200

    added view

---
+++ server.js
@@ -0,0 +1,32 @@
+const _ = require('lodash');
+//import { Express } from 'express';
+const express = require('express');
+
+const app = express();
+
+//register view engine 
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    const blogs = [
+        {title: 'Yosh finds eggs', snippet: "Lorem ipsum dolor sit amet consectetur"},
+        {title: 'Mario finds stars', snippet: "Lorem ipsum dolor sit amet consectetur"},
+        {title: 'How to defeat browser', snippet: "Lorem ipsum dolor sit amet consectetur"}
+    ]
+    res.render('index',{title: "Home" , blogs});
+})
+app.get('/about', (req,res)=>{
+    res.render('about',{title: "About"});
+});
+
+app.get('/blogs/create', (req,res)=>{
+    res.render('create',{title: "Create a new Blog"})
+})
+
+app.use((req,res)=>{
+    res.status(404).render('404',{title: "404"})
+})
+
+//
+const port = process.env.PORT || 5000; 
+app.listen(port , ()=> console.log(`We're running on port ${port}...`));
